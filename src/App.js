3d27a1a3d70f62551a3086fb72fb78bf4f9e3717
christian bonet <christianm.bonet@gmail.com>
2022-03-15 17:54:49 -0400

    making edits to routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
          <Router>
                 <Routes>
                   <Route className='navigation' element={<NavBar/>}/>
-                  <Route exact path='/' element={<DataFetch/>}/>
+                  <Route exact path='/'element={<DataFetch/>}/>
                 </Routes>
             </Router>
       </div>
